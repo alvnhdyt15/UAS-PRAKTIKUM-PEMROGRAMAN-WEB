1a0ea1348960cea8b0964c93c6f3082b9f6294be
alvnhdyt15 <alvinhidayattullab@gmail.com>
2026-07-01 19:57:31 +0700

    Add files via upload
    Pengaturan variabel konfigurasi basis data dan konstanta BASEURL pada config/config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+// config/config.php
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'madura_db');
+
+define('BASE_URL', 'http://localhost/madura/public');
+define('APP_NAME', 'MDEWS (Madura Disaster Early Warning System)');
